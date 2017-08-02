143e2de87dfa307c9c88383591e8f4a5855e4284
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2017-08-02 20:51:04 +0200

    Add more custom layouts

--- pagenotfound.php
+++
@@ -1,9 +0,0 @@
-<?php // @Layout name: Forbidden ?>
-<?php echo ipView('_header.php', array('headerClass' => 'above'))->render(); ?>
-
-<main class="errors">
-    <h1>Page Not Found</h1>
-    <p>Cannot find the page you're looking for</p>
-</main>
-
-<?php echo ipView('_footer.php')->render(); ?>
---
+++ profile.php
@@ -0,0 +1,26 @@
+<?php // @Layout name: Profile ?>
+
+<?php
+$hasPageImage = false;
+
+if (in_array('PageImage', \Ip\Internal\Plugins\Service::getActivePluginNames())) {
+    $hasPageImage = !empty(\Plugin\PageImage\Model::getPageImages(ipContent()->getCurrentPage()->getId()));
+}
+?>
+
+<?php echo ipView('_header.php', array('headerClass' => ''))->render(); ?>
+
+<main class="profile">
+    <?php if ($hasPageImage): ?>
+        <div class="banner">
+            <div class="image-container darkest<?= !empty($useBlur) ? 'blurred' : '' ?>">
+                <div class="image" <?php echo ipSlot('bkgImage'); ?>></div>
+            </div>
+            <?= ipBlock('banner') ?>
+        </div>
+    <?php endif; ?>
+
+    <?php echo ipBlock('main'); ?>
+</main>
+
+<?php echo ipView('_footer.php')->render(); ?>
