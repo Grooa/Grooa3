658dac8fbb39337ea065e9abb0710608658c7f5a
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-06-14 14:25:22 +0200

    Add slick-slider CSS content

--- _header.php
+++ _header.php
@@ -21,9 +21,13 @@ if (in_array('PageImage', \Ip\Internal\Plugins\Service::getActivePluginNames()) 
 <html<?php echo ipHtmlAttributes(); ?>>
 
 <head>
+    <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+
     <?php ipAddCss('assets/theme.css'); ?>
     <?php ipAddJs('assets/theme.js'); ?>
     <?php echo ipHead(); ?>
+
     <?php if (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== false) { ?>
         <style type="text/css">
             .cover {
