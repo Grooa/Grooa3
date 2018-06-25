bbbfeb7099aeac20cfc403b8e3ac138a3af46a26
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-06-25 20:13:49 +0200

    Refractor login and registration pages

--- override/Plugin/User/view/login.php
+++ override/Plugin/User/view/login.php
@@ -1,22 +1,23 @@
-<section class="ipWidget">
-    <?php echo ipRenderWidget('Heading', array('title' => __('Login', 'User', false))) ?>
+<section class="ipWidget ipWidget-UserHeader">
+    <?php echo ipRenderWidget('Heading', array('title' => __('Welcome back', 'User', false))) ?>
     <p class="center introduction">
         Login to access your purchased online courses,
-        manage your email subscription and profile details
+        manage your email subscription and other profile details.
     </p>
 </section>
 
-<section class="ipWidget object">
-    <?php echo $form->render() ?>
+<section class="ipWidget ipWidget-LoginForm">
+    <?= $form->render() ?>
 
-    <section class="horizontal">
-        <p class="centered">
-            <a class="ipUserRegistrationLink" href="<?= escAttr($registrationUrl) ?>"><?php _e('Register', 'User') ?></a>
-        </p>
-        <p class="centered">
-            <a class="ipUserPasswordResetLink" href="<?= escAttr($passwordResetUrl) ?>"><?php _e('Forgot password', 'User') ?></a>
-        </p>
-    </section>
+    <p>
+        <a class="ipUserPasswordResetLink" href="<?= escAttr($passwordResetUrl) ?>">
+            <?php _e('Forgot password', 'User') ?>
+        </a>
+    </p>
+
+    <p>
+        Don't have an account? <a href="<?= escAttr($registrationUrl) ?>"><?= _e('Sign up for free', 'User') ?></a>
+    </p>
 </section>
 
 <div class="modern-spacing" style="height: 30px"></div>
--- override/Plugin/User/view/registration.php
+++ override/Plugin/User/view/registration.php
@@ -1,11 +1,16 @@
-<?php echo ipRenderWidget('Heading', array('title' => __('Registration', 'User', false))); ?>
 
-<?= ipBlock('main')->render() ?>
+<?php echo ipRenderWidget('Heading', array('title' => __('Create an account', 'User', false))); ?>
 
-<section class="ipWidget object">
-    <?php echo $form->render() ?>
-    <a href="/terms-and-conditions">Terms & conditions</a>
-	<p>We will NEVER spam, rent, sell or share your e-mail address.</p>
-</section>
+<div class="columns spaced topped">
+    <div class="ipWidget ipWidget-RegisterForm">
+        <?php echo $form->render() ?>
+        <a href="/terms-and-conditions">Terms & conditions</a>
+        <p>We will NEVER spam, rent, sell or share your e-mail address.</p>
+    </div>
+
+    <div>
+        <?= ipBlock('main') ?>
+    </div>
+</div>
 
 <?= ipBlock('bellow')->render() ?>
